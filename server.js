e23fb14f0d4e9cd2bb55e2dac32414fd8b5969cb
vfuentes1213@gmail.com <vfuentes1213@gmail.com>
2021-07-31 16:36:03 -0600

    add api docs route

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const app = express()
 // TODO use env var
 const port = 8000
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./openapi.yml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/v1/message', (req, res) => {
     res.send({data: [{payload: "msg content"}]})
 })
@@ -10,6 +16,7 @@ app.get('/v1/message', (req, res) => {
 app.post("/v1/message", (req, res) => {
     res.send({ message: "ok", "id": ""});
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
